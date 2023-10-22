d92cac83a87f18d15e45679f614c180978dadbee
Aya <ayaelrhayour123@gmail.com>
2023-10-23 00:33:43 +0100

    working on animations

---
+++ js/faq.js
@@ -0,0 +1,8 @@
+const accordions = document.querySelectorAll('.questions');
+accordions.forEach(item => {
+    const header = item.querySelector(".question");
+    header.addEventListener("click", () =>{
+        item.classList.toggle("active");
+    })
+})
+
